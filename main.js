e8fc36b88e411d54277202f890edf62b101d45d9
JosiahPhua <josiahphuajy@gmail.com>
2021-05-17 21:00:54 +0800

    not sure why my DOM is running an error.

---
+++ main.js
@@ -0,0 +1,31 @@
+document.getElementById("guidebooks").addEventListener("click", function(){
+    alert("LOL Don't Have.")
+});
+
+document.getElementById("locations").addEventListener("click", function(){
+    alert("LOL Don't Have.")
+});
+
+document.getElementById("box1").addEventListener("click", function(){
+    window.open("www.google.com")
+});
+
+document.getElementById("box2").addEventListener("click", function(){
+    window.open("www.google.com")
+});
+
+document.getElementById("box3").addEventListener("click", function(){
+    window.open("www.google.com")
+});
+
+document.getElementById("box4").addEventListener("click", function(){
+    window.open("www.google.com")
+});
+
+document.getElementById("box5").addEventListener("click", function(){
+    window.open("www.google.com")
+});
+
+document.getElementById("box6").addEventListener("click", function(){
+    window.open("www.google.com")
+});
