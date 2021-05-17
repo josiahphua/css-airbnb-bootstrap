16c0523937df9ffe584ce70ae1f5956c45011891
JosiahPhua <josiahphuajy@gmail.com>
2021-05-17 21:09:39 +0800

    Suddenly everything works

--- main.js
+++ main.js
@@ -7,25 +7,25 @@ document.getElementById("locations").addEventListener("click", function(){
 });
 
 document.getElementById("box1").addEventListener("click", function(){
-    window.open("www.google.com")
+    window.open("https://www.google.com.sg/maps/place/San+Francisco,+CA,+USA/@37.7576948,-122.4726194,13z/data=!3m1!4b1!4m5!3m4!1s0x80859a6d00690021:0x4a501367f076adff!8m2!3d37.7749295!4d-122.4194155")
 });
 
 document.getElementById("box2").addEventListener("click", function(){
-    window.open("www.google.com")
+    window.open("https://www.google.com.sg/maps/place/New+York,+NY,+USA/@40.6974034,-74.1197634,11z/data=!3m1!4b1!4m5!3m4!1s0x89c24fa5d33f083b:0xc80b8f06e177fe62!8m2!3d40.7127753!4d-74.0059728")
 });
 
 document.getElementById("box3").addEventListener("click", function(){
-    window.open("www.google.com")
+    window.open("https://www.google.com.sg/maps/place/London,+UK/@51.5285582,-0.2416797,11z/data=!3m1!4b1!4m5!3m4!1s0x47d8a00baf21de75:0x52963a5addd52a99!8m2!3d51.5073509!4d-0.1277583")
 });
 
 document.getElementById("box4").addEventListener("click", function(){
-    window.open("www.google.com")
+    window.open("https://www.google.com.sg/maps/place/Napa+Valley,+CA,+USA/@38.4297365,-122.5609137,11z/data=!3m1!4b1!4m5!3m4!1s0x808454f53baeb1ad:0x863a3784a09da6e3!8m2!3d38.4274315!4d-122.3943299")
 });
 
 document.getElementById("box5").addEventListener("click", function(){
-    window.open("www.google.com")
+    window.open("https://www.google.com.sg/maps/place/Sonoma,+CA+95476,+USA/@38.291092,-122.4666091,14z/data=!3m1!4b1!4m5!3m4!1s0x8085ac43817b9d77:0x380ed210ee6e045!8m2!3d38.291859!4d-122.4580356")
 });
 
 document.getElementById("box6").addEventListener("click", function(){
-    window.open("www.google.com")
+    window.open("https://www.google.com.sg/maps/place/San+Francisco,+CA,+USA/@37.7576948,-122.4726194,13z/data=!3m1!4b1!4m5!3m4!1s0x80859a6d00690021:0x4a501367f076adff!8m2!3d37.7749295!4d-122.4194155")
 });
